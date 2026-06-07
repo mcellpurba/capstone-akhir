329dd2b3eb97ac0cbc8d7c77554c489d18da021d
marcellino-elrobson-purba <elrobson125@gmail.com>
2026-06-08 07:49:20 +0800

    ganti nama

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -116,7 +116,7 @@ function Home() {
             </div>
             <div className="about-card-content">
               <h3 className="member-name">Rizky</h3>
-              <p className="member-role">Data Science</p>
+              <p className="member-role">Data Scientist</p>
               <div className="member-socials">
                 <a href="https://www.linkedin.com/in/muhammadrizkyramadhan-sanken/" className="social-icon">
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
@@ -139,7 +139,7 @@ function Home() {
             </div>
             <div className="about-card-content">
               <h3 className="member-name">Haba</h3>
-              <p className="member-role">Data Science</p>
+              <p className="member-role">Data Scientist</p>
               <div className="member-socials">
                 <a href="https://www.linkedin.com/in/haba-l-herlambang-banjarnahor-793524289/" className="social-icon">
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
